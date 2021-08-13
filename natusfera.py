058eec6caaa256bca9f876afce35a8dfacc2787c
anomalia <anavgo@gmail.com>
2021-08-13 19:43:46 +0200

    unificacion de funciones en get_obs y get_project,

--- natusfera.py
+++ natusfera.py
@@ -1,160 +1,42 @@
 #!/usr/bin/env python3
-from typing import List, Union
+from typing import List, Union, Optional
 import requests
 from bs4 import BeautifulSoup
 import datetime
+from typing import Dict, List
 
 API_URL = "https://natusfera.gbif.es"
 
-def get_project_from_id(id:int) -> dict:
-    url = f"{API_URL}/projects/{id}.json"
-    page = requests.get(url, verify=False)
-    
-    return page.json()
+TAXONS = [
+    "Chromista",
+    "Protozoa",
+    "Animalia",
+    "Mollusca",
+    "Arachnida",
+    "Insecta",
+    "Aves",
+    "Mammalia",
+    "Amphibia",
+    "Reptilia",
+    "Actinopterygii",
+    "Fungi",
+    "Plantae",
+    "Unknown"
+]
 
-def convert_to_datetime(observations: Union[dict, list]) -> list:
+def _convert_to_datetime(observations: list) -> list:
     campos = ["created_at", "observed_on", "updated_at"]
-    
-    if type(observations) is dict:
+
+    for observation in observations:
         for campo in campos:
             try:
-                if type(observations[campo]) != datetime.datetime:
-                    observations[campo] = datetime.datetime.fromisoformat(observations[campo])
+                if type(observation[campo]) != datetime.datetime:
+                    observation[campo] = datetime.datetime.fromisoformat(observation[campo])
             except KeyError:
                 pass
-    elif type(observations) is list:
-        for observation in observations:
-            for campo in campos:
-                try:
-                    if type(observation[campo]) != datetime.datetime:
-                        observation[campo] = datetime.datetime.fromisoformat(observation[campo])
-                except KeyError:
-                    pass
     return observations
 
-
-def get_obs_from_query(query:str) -> list:
-    observations = []
-    n = 1
-    url = f'{API_URL}/observations.json?q="{query}"&per_page=200&page={n}'
-    page = requests.get(url)
-    if len(requests.get(f'{API_URL}/observations.json?q="{query}"&per_page=200&page=100').json()) < 200:
-        while len(page.json()) == 200:
-            observations.extend(page.json())
-            n += 1
-            url = f'{API_URL}/observations.json?q="{query}"&per_page=200&page={n}'
-            page = requests.get(url)
-        
-        observations.extend(page.json())
-    else:
-        raise ValueError("Number of results out of range. Need to add more filters")
-            
-    #__import__("pdb").set_trace()
-    
-    observations = convert_to_datetime(observations)
-
-    return observations
-
-def get_obs_from_id(id:int) -> dict:
-    """Get information on a specific observation given an id"""
-
-    url = f'{API_URL}/observations/{id}.json'
-    page = requests.get(url)
-    
-    observation = convert_to_datetime(page.json())
-
-    return observation
-
-def get_obs_from_user(user:str) -> list:
-    """Download observations for a user"""
-    observations = []
-    n = 1
-    url = f'{API_URL}/observations/{user}.json?per_page=200&page={n}'
-    page = requests.get(url)
-        
-    while len(page.json()) == 200:
-        observations.extend(page.json())
-        n += 1
-        url = f'{API_URL}/observations/{user}.json?per_page=200&page={n}'
-        page = requests.get(url)
-        
-    observations.extend(page.json())
-
-    observations = convert_to_datetime(observations)
-
-    #__import__("pdb").set_trace()
-
-    return observations
-
-def get_obs_from_project(id:int) -> list:
-    """Download observations or info from a project"""
-    observations = []
-    n = 1
-    url = f"{API_URL}/observations/project/{id}.json?per_page=200&page={n}"
-    page = requests.get(url)
-
-    while len(page.json()) == 200:
-        observations.extend(page.json())
-        n += 1
-        url = f"{API_URL}/observations/project/{id}.json?per_page=200&page={n}"
-        page = requests.get(url)
-    
-    observations.extend(page.json())
-
-    observations = convert_to_datetime(observations)
-
-    return observations
-
-def get_project_from_name(name:str) -> dict:
-    """Download information of projects from name"""  
-    
-    url = f"{API_URL}/projects/search.json?q={name}"
-    page = requests.get(url, verify=False)
-    
-    resultado = convert_to_datetime(page.json())
-
-    return resultado
-
-def get_obs_from_taxon(taxon:str) -> list:
-    taxon_names = [
-        "Chromista",
-        "Protozoa",
-        "Animalia",
-        "Mollusca",
-        "Arachnida",
-        "Insecta",
-        "Aves",
-        "Mammalia",
-        "Amphibia",
-        "Reptilia",
-        "Actinopterygii",
-        "Fungi",
-        "Plantae",
-        "unknown"
-    ]
-
-    if taxon in taxon_names:
-        observations = []
-        n = 1
-        url = f"{API_URL}/observations.json?iconic_taxa={taxon}&per_page=200&page={n}"
-        page = requests.get(url, verify=False)
-
-        while len(page.json()) == 200:
-            observations.extend(page.json())
-            n += 1
-            url = f"{API_URL}/observations.json?iconic_taxa={taxon}&per_page=200&page={n}"
-            page = requests.get(url, verify=False)
-
-        observations.extend(page.json())
-        
-        observations = convert_to_datetime(observations)
-        
-        return observations
-
-    else:
-        print("No es una taxonomía válida")
-
-def get_ids_from_place(place:str) -> list:
+def _get_ids_from_place(place:str) -> list:
     place_ids = []
     url = f"{API_URL}/places.json?q={place}"
     page = requests.get(url, verify=False)
@@ -164,53 +46,121 @@ def get_ids_from_place(place:str) -> list:
         place_ids.append(place_id)
     return place_ids
 
-def get_obs_from_place_name(place:str) -> list:
-    
-    place_ids = get_ids_from_place(place)
-    
-    #__import__("pdb").set_trace()
-    
-    observations = []
-    for place_id in place_ids:
-        n = 1
-        url = f"{API_URL}/observations.json?place_id={place_id}&per_page=200&page={n}"
-        page = requests.get(url, verify=False)
-        if len(page.json()) > 0:
-            while len(page.json()) == 200:
-                observations.extend(page.json())
-                n += 1
-                url = f"{API_URL}/observations.json?place_id={place_id}&per_page=200&page={n}"
-                page = requests.get(url, verify=False)
+def get_project(p: Union[str, int]) -> Dict:
+    """Download information of a project from id or name"""  
 
-            observations.extend(page.json())
+    if type(p) is int:
+        url = f"{API_URL}/projects/{p}.json"
+    elif type(p) is str:
+        url = f"{API_URL}/projects/search.json?q={p}"
 
-    observations = convert_to_datetime(observations)
-
-    for observation in observations:
-        observation['place_id'] = place_id
-        
-    return observations
-    
-def get_obs_from_place_id(place_id:int) -> list:
-    
-    n = 1
-    observations = []
-    
-    url = f"{API_URL}/observations.json?place_id={place_id}&per_page=200&page={n}"
     page = requests.get(url, verify=False)
-    if len(page.json()) > 0:
-        while len(page.json()) == 200:
-            observations.extend(page.json())
-            n += 1
-            url = f"{API_URL}/observations.json?place_id={place_id}&per_page=200&page={n}"
-            page = requests.get(url, verify=False)
+    resultado = page.json()
+    
+    campos = ["created_at", "observed_on", "updated_at"]
+    for campo in campos:
+        try:
+            if type(resultado[campo]) != datetime.datetime:
+                resultado[campo] = datetime.datetime.fromisoformat(resultado[campo])
+        except KeyError:
+            pass
 
-        observations.extend(page.json())
-        observations = convert_to_datetime(observations)    
+    return resultado
 
+def _build_url(
+    query: Optional[str] = None, 
+    id_project: Optional[int] = None,
+    id_obs: Optional[int] = None,
+    user: Optional[str] = None,
+    taxon: Optional[str] = None,
+    place_id: Optional[int] = None,
+    ) -> str:
+
+    # definir la base url
+    if id_project is not None:
+        base_url = f"{API_URL}/observations/project/{id_project}.json"
+    elif id_obs is not None:
+        base_url = f"{API_URL}/observations/{id_obs}.json"
+    elif user is not None:
+        base_url = f"{API_URL}/observations/{user}.json"
     else:
-        print("No existen observaciones para este id de lugar")
+        base_url = f"{API_URL}/observations.json"
+    
+    # definir los argumentos que admite la API
+    args = []
+    if query is not None:
+        args.append(f'q="{query}"')
+    if taxon is not None:
+        taxon = taxon.title()
+        if taxon in TAXONS:
+            args.append(f"iconic_taxa={taxon}")
+        else:
+            raise ValueError("No es una taxonomía válida")
+    if place_id is not None:
+        args.append(f"place_id={place_id}")
+
+    
+    url = f'{base_url}?{"&".join(args)}&per_page=200'
+
+    return url
+
+def _request(arg_url: str) -> List:
+    observations = []
+    n = 1
+    page = requests.get(arg_url)
+    #__import__("pdb").set_trace()
+
+    while len(page.json()) == 200:
+        
+        n += 1
+        if n > 100:
+            print("WARNING: Only the first 20,000 results are displayed")
+            break
+        observations.extend(page.json())
+        url = f"{arg_url}&page={n}"
+        page = requests.get(url)
+    
+    if type(page.json()) == list:
+        observations.extend(page.json())
+    else:
+        observations.append(page.json())
     
     return observations
 
-#get_inat_obs_project(): Download observations or info from a project
+def get_obs(
+    query: Optional[str] = None, 
+    id_project: Optional[int] = None,
+    id_obs: Optional[int] = None,
+    user: Optional[str] = None,
+    taxon: Optional[str] = None,
+    place_id: Optional[int] = None,
+    place_name: Optional[str] = None,
+    ) -> List:
+
+    if place_name is not None:
+        place_ids = _get_ids_from_place(place_name)
+        observations = []
+        for place_id in place_ids:
+            url = _build_url(
+                query, 
+                id_project,
+                id_obs,
+                user,
+                taxon,
+                place_id,
+                )
+            observations.extend(_request(url))
+    else:
+        url = _build_url(
+            query, 
+            id_project,
+            id_obs,
+            user,
+            taxon,
+            place_id,
+            )
+
+        observations = _request(url)
+
+    return _convert_to_datetime(observations)
+
--- test_natusfera.py
+++ test_natusfera.py
@@ -6,16 +6,8 @@ import json
 import datetime
 import pytest
 from .natusfera import (
-    get_project_from_id,
-    get_obs_from_query,
-    get_obs_from_id,
-    get_obs_from_user,  
-    get_obs_from_project,
-    get_project_from_name,
-    get_obs_from_taxon,
-    get_ids_from_place,
-    get_obs_from_place_id,
-    get_obs_from_place_name
+    get_project,
+    get_obs
 )
 
 API_URL = "https://natusfera.gbif.es"
@@ -30,18 +22,15 @@ def test_get_project_from_id_extract_observation_data(requests_mock):
             'taxon': 'Fungi',
         }
     )
-
-    result = get_project_from_id(806)
+    result = get_project(806)
 
     assert result['id'] == 806
     assert result['taxon'] == 'Fungi'
 
-    # assert result['id'] == 806
-
 
 def test_get_obs_from_query_returns_observations_data_when_less_than_pagination(requests_mock):
     requests_mock.get(
-        f'{API_URL}/observations.json?q="quercus quercus"&per_page=200&page=1',
+        f'{API_URL}/observations.json?q="quercus quercus"&per_page=200',
         json=[{
             "id": id, 
             "taxon_id": 3, 
@@ -52,18 +41,18 @@ def test_get_obs_from_query_returns_observations_data_when_less_than_pagination(
         json=[],
     )
 
-    result = get_obs_from_query("quercus quercus")
+    result = get_obs(query="quercus quercus")
 
     assert len(result) == 100
     assert result[0]["id"] == 0
     assert result[0]["taxon_id"] == 3
     assert type(result[0]["created_at"]) == datetime.datetime
     
-def test_get_observations_returns_observations_data_when_more_than_pagination(
+def test_get_obs_returns_observations_data_when_more_than_pagination(
     requests_mock,
     ) -> None:
     requests_mock.get(
-        f'{API_URL}/observations.json?q="quercus quercus"&per_page=200&page=1',
+        f'{API_URL}/observations.json?q="quercus quercus"&per_page=200',
         json=[{"id": id_, "taxon_id": 3} for id_ in range(200)],
     )
     requests_mock.get(
@@ -75,23 +64,31 @@ def test_get_observations_returns_observations_data_when_more_than_pagination(
         json=[],
     )
 
-    result = get_obs_from_query("quercus quercus")
+    result = get_obs(query="quercus quercus")
 
     assert len(result) == 250
     assert result[0]["id"] == 0
     assert result[0]["taxon_id"] == 3
 
 
-def test_get_observations_returns_error_when_more_than_20000_results(
+def test_get_obs_returns_error_when_more_than_20000_results(
     requests_mock,
+    capsys,
     ) -> None:
     """The API will return an error."""
-    for page in range(1, 101):
+    requests_mock.get(
+        f'{API_URL}/observations.json?q="quercus quercus"&per_page=200',
+        json=[
+            {"id": id_, "taxon_id": 3}
+            for id_ in range(0, 200)
+        ],
+    )
+    for page in range(2, 100):
         requests_mock.get(
             f'{API_URL}/observations.json?q="quercus quercus"&per_page=200&page={page}',
             json=[
                 {"id": id_, "taxon_id": 3}
-                for id_ in range(200 * (page - 1), 200 * page + 1)
+                for id_ in range(200 * (page - 1), 200 * page)
             ],
         )
     requests_mock.get(
@@ -99,14 +96,18 @@ def test_get_observations_returns_error_when_more_than_20000_results(
         json={"message": "You reach 20,000 items limit"},
     )
 
-    with pytest.raises(
-        ValueError,
-        match="Number of results out of range. Need to add more filters"
-    ):
-        get_obs_from_query("quercus quercus")
+    result = get_obs("quercus quercus")
+
+    # captura el mensaje de error que aparece en el output
+    out, err = capsys.readouterr()
+    
+    assert len(result) == 20000
+    assert "WARNING: Only the first 20,000 results are displayed" in out
+    
+    
 
 
-def test_get_observations_by_id_returns_observations_data(
+def test_get_obs_by_id_returns_observations_data(
     requests_mock,
     ) -> None:
     requests_mock.get(
@@ -120,20 +121,20 @@ def test_get_observations_by_id_returns_observations_data(
         },
     )
 
-    result = get_obs_from_id(2084)
-
-    assert result["id"] == 2084
-    assert result["taxon_id"] == 3
-    assert result['created_at'] == datetime.datetime(2016, 5, 1, 6, 32, 3, tzinfo=datetime.timezone(datetime.timedelta(days=-1, seconds=50400)))
-    assert result['observed_on'] == datetime.datetime(2016, 5, 1, 0, 0)
-    assert result['updated_at'] == datetime.datetime(2021, 3, 15, 1, 19, 22, tzinfo=datetime.timezone(datetime.timedelta(days=-1, seconds=50400)))
+    result = get_obs(id_obs=2084)
+    assert len(result) == 1
+    assert result[0]["id"] == 2084
+    assert result[0]["taxon_id"] == 3
+    assert result[0]['created_at'] == datetime.datetime(2016, 5, 1, 6, 32, 3, tzinfo=datetime.timezone(datetime.timedelta(days=-1, seconds=50400)))
+    assert result[0]['observed_on'] == datetime.datetime(2016, 5, 1, 0, 0)
+    assert result[0]['updated_at'] == datetime.datetime(2021, 3, 15, 1, 19, 22, tzinfo=datetime.timezone(datetime.timedelta(days=-1, seconds=50400)))
 
 
 def test_get_obs_from_user_returns_observations_data(
     requests_mock,
     ) -> None:
     requests_mock.get(
-        f"{API_URL}/observations/zolople.json?per_page=200&page=1",
+        f"{API_URL}/observations/zolople.json?per_page=200",
         json=[
             {"user_login": "zolople", 
             "id": 2084, 
@@ -147,7 +148,7 @@ def test_get_obs_from_user_returns_observations_data(
             "taxon_id": 3} for id_ in range(200, 260)],
     )
     
-    result = get_obs_from_user("zolople")
+    result = get_obs(user="zolople")
     #__import__("pdb").set_trace()
 
     assert len(result) == 260
@@ -158,13 +159,13 @@ def test_get_obs_from_user_returns_observations_data(
 
 def test_get_obs_project_returns_observations_data(requests_mock,) -> None:
     requests_mock.get(
-        f"{API_URL}/observations/project/806.json?per_page=200&page=1",
+        f"{API_URL}/observations/project/806.json?per_page=200",
         json=[
             {"user_login": "zolople", 
             "id": 2084, 
             "taxon_id": 3} for id_ in range(37)],
     )
-    result = get_obs_from_project(806)
+    result = get_obs(id_project=806)
     
     assert len(result) == 37
 
@@ -181,7 +182,7 @@ def test_get_project_from_name_returns_observations_data(requests_mock,) -> None
         }
     )
 
-    result = get_project_from_name('urbamar')
+    result = get_project('urbamar')
 
     assert result['id'] == 1191
     assert len(result) == 6
@@ -189,7 +190,7 @@ def test_get_project_from_name_returns_observations_data(requests_mock,) -> None
 
 def test_get_obs_from_taxon_returns_info_with_pagination(requests_mock,) -> None:
     requests_mock.get(
-        f"{API_URL}/observations.json?iconic_taxa=Fungi&per_page=200&page=1",
+        f"{API_URL}/observations.json?iconic_taxa=Fungi&per_page=200",
         json=[{
             "taxon": "Fungi", 
             "id": 1645, 
@@ -215,43 +216,15 @@ def test_get_obs_from_taxon_returns_info_with_pagination(requests_mock,) -> None
             'updated_at': '2020-09-26T05:07:36-10:00',} for id_ in range(56)
         ]
     )
-    result = get_obs_from_taxon('Fungi')
+    result = get_obs(taxon='Fungi')
 
     assert len(result) == 456
     assert type(result[0]['updated_at']) == datetime.datetime
 
 
-def test_get_ids_from_place_returns_list_ids(requests_mock,) -> None:
-    requests_mock.get(
-        f"{API_URL}/places.json?q=Barcelona",
-        json=[{'display_name': 'Barcelona, Catalonia, ES',
-                'id': 20, 
-                'name': 'Barcelona'},
-            {'display_name': 'Barcelona, Catalonia, ES',
-                'id': 66,
-                'name': 'Barcelona'},
-            {'display_name': 'Barcelona, Catalonia, ES',
-                'id': 67,
-                'name': 'Barcelona'},
-            {'display_name': 'Barcelona, Catalonia, ES',
-            'id': 1011,
-            'name': 'Barcelona'},
-            {'display_name': 'Barcelona, Catalonia, ES',
-            'id': 424,
-            'name': 'Barcelona'},
-            {'display_name': 'Barcelona, Catalonia, ES',
-            'id': 425,
-            'name': 'Barcelona'}]
-            )
-    result = get_ids_from_place("Barcelona")
-    
-    assert len(result) == 6
-    assert type(result) == list
-    assert 424 in result
-
 def test_get_obs_from_place_id_returns_obs(requests_mock,) -> None:
     requests_mock.get(
-        f"{API_URL}/observations.json?place_id=1011&per_page=200&page=1",
+        f"{API_URL}/observations.json?place_id=1011&per_page=200",
         json=[{
             "taxon": "Fungi", 
             "id": 1645, 
@@ -277,12 +250,11 @@ def test_get_obs_from_place_id_returns_obs(requests_mock,) -> None:
             'updated_at': '2020-09-26T05:07:36-10:00',} for id_ in range(56)
         ]
     )
-    result = get_obs_from_place_id(1011)
+    result = get_obs(place_id=1011)
     assert len(result) == 456
     assert type(result[0]['updated_at']) == datetime.datetime
 
 def test_get_obs_from_place_name_returns_obs(requests_mock,) -> None:
-    
     requests_mock.get(
         f"{API_URL}/places.json?q=Barcelona",
         json=[{'id': 20}, {'id': 67}, {'id': 1024}]
@@ -311,7 +283,7 @@ def test_get_obs_from_place_name_returns_obs(requests_mock,) -> None:
         )
     
 
-    result = get_obs_from_place_name("Barcelona")
+    result = get_obs(place_name="Barcelona")
     
     assert len(result) > 200
 
@@ -321,3 +293,79 @@ def test_get_obs_from_place_name_returns_obs(requests_mock,) -> None:
     assert len(place_ids) > 1
 
     assert type(result[0]['updated_at']) == datetime.datetime
+
+# test para nombre de place_name que no devuelve nada
+def test_get_obs_from_place_name_returns_obs(requests_mock,) -> None:
+    requests_mock.get(
+        f"{API_URL}/places.json?q=Cuntis",
+        json=[]
+        )
+    
+    result = get_obs(place_name="Cuntis")
+    
+    assert len(result) == 0
+
+# test de uso de la función con taxon en minúsculas
+def test_get_obs_from_taxon_min_returns_info(requests_mock,) -> None:
+    requests_mock.get(
+        f"{API_URL}/observations.json?iconic_taxa=Fungi&per_page=200",
+        json=[{
+            "taxon": "Fungi", 
+            "id": 1645, 
+            "taxon_id": 3,
+            'updated_at': '2020-09-26T05:07:36-10:00',} for id_ in range(200)
+            ]
+    )
+    requests_mock.get(
+        f"{API_URL}/observations.json?iconic_taxa=Fungi&per_page=200&page=2",
+        json=[{
+            "taxon": "Fungi", 
+            "id": 1645, 
+            "taxon_id": 3,
+            'updated_at': '2020-09-26T05:07:36-10:00',} for id_ in range(200)
+        ]
+    )
+    requests_mock.get(
+        f"{API_URL}/observations.json?iconic_taxa=Fungi&per_page=200&page=3",
+        json=[{
+            "taxon": "Fungi", 
+            "id": 1645, 
+            "taxon_id": 3,
+            'updated_at': '2020-09-26T05:07:36-10:00',} for id_ in range(56)
+        ]
+    )
+    result = get_obs(taxon='fungi')
+
+    assert len(result) == 456
+    assert type(result[0]['updated_at']) == datetime.datetime
+
+# test de usos combinados
+def test_get_obs_from_combined_arguments(requests_mock,) -> None:
+    requests_mock.get(
+        f"{API_URL}/observations/zolople.json?iconic_taxa=Mollusca&per_page=200",
+        json=[{
+            "taxon": "Mollusca",
+            "id": id_,
+            "taxon_id": 3,
+            "user": "zolople",
+            "created_at": '2020-09-26',} for id_ in range(5)
+            ]
+    )
+    result = get_obs(taxon="Mollusca", user="zolople")
+
+    assert len(result) == 5
+    assert result[0]['created_at'] == datetime.datetime(2020, 9, 26)
+
+# test combinado id_project, place_id, query
+def test_get_obs_from_three_combined_arguments(requests_mock,) -> None:
+    requests_mock.get(
+        f'{API_URL}/observations/project/45.json?place_id=3&q="quercus quercus"&per_page=200',
+        json=[
+            {"id": 4586, "project": 45, "place": 3, "species": "quercus quercus"},
+            {"id": 4588, "project": 45, "place": 3, "species": "quercus quercus"},
+        ]
+    )
+    result = get_obs(id_project=45, place_id=3, query="quercus quercus")
+
+    assert len(result) == 2
+
