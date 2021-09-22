7290dadc0c084057db62f89dde8c8892addcf125
anomalia <anavgo@gmail.com>
2021-09-22 20:16:33 +0200

    actualizado

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="natusfera",
-    version="0.4.1",
+    version="0.4.2",
     description="Librería para extraer información recogida en la API Natusfera.",
     author="Ana Alvarez",
     author_email="anomalia@disroot.org",
